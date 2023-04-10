dbe386caee46e92d7fc076e4e54fb219e6cfb44f
Dmytro Beridze <moderatefast@gmail.com>
2023-04-10 21:41:05 +0300

    layout modal window HTML CSS

--- src/assets/js/_main_icons_module.js
+++ src/assets/js/_main_icons_module.js
@@ -1,70 +1,45 @@
 const main_icons_module = () => {
-  const mainInfoOptions = document.querySelector(".main__info_options");
+  const mainIconsTitles = [
+    "висока якість",
+    "швидкий монтаж",
+    "гарантія 3 роки",
+    "вивіз сміття",
+  ];
 
-  const mainIconsTitles = {
-    quality: {
-      1: "висока",
-      2: "якість",
-    },
+  const mainIcons = {
+    "висока якість": "quality",
+    "швидкий монтаж": "time",
+    "гарантія 3 роки": "quality1",
+    "вивіз сміття": "delivery",
   };
 
   class Main_icons_module {
-    constructor(firstData, secondData) {
-      this.first = firstData;
-      this.second = secondData;
+    constructor(data, icons, containerSelector) {
+      this.containerSelector = containerSelector;
+      this.data = data;
+      this.icons = icons;
     }
 
     renderIcon() {
-      let iconsBlock = `<div class="options__icon-container">
-        <img src="./img/main/icons/quality.png" alt="option">
-        <div class="options__icon_title" >
-        <span>Висока </span>
-        <span>якість </span>
-        </div>
-        </div>`;
+      const mainInfoOptions = document.querySelector(this.containerSelector);
+      this.data.forEach((element) => {
+        let iconsBlock = `<div class="options__icon-container">
+            <img src="./img/main/icons/${this.icons[element]}.png" alt="option">
+            <div class="options__icon_title" >
+            ${element} 
+            </div>
+            </div>`;
 
-      mainInfoOptions.innerHTML += iconsBlock;
+        mainInfoOptions.innerHTML += iconsBlock;
+      });
     }
   }
 
   let testOne = new Main_icons_module(
-    mainIconsTitles.quality[1],
-    mainIconsTitles.quality[2]
+    mainIconsTitles,
+    mainIcons,
+    ".main__info_options"
   );
   testOne.renderIcon();
-
-  // !
-  //   const mainIconsTitles = {
-  //     quality: {
-  //       1: "висока",
-  //       2: "якість",
-  //     },
-  //   };
-
-  //   class Main_icons_module {
-  //     constructor(firstData, secondData) {
-  //       this.first = firstData;
-  //       this.second = secondData;
-  //     }
-
-  //     renderIcon() {
-  //       let iconsBlock = `<div class="options__icon-container">
-  //         <img src="./img/main/icons/quality.png" alt="option">
-  //         <div class="options__icon_title" >
-  //         <span>Висока </span>
-  //         <span>якість </span>
-  //         </div>
-  //         </div>`;
-
-  //       mainInfoOptions.innerHTML += iconsBlock;
-  //     }
-  //   }
-
-  //   let testOne = new Main_icons_module(
-  //     mainIconsTitles.quality[1],
-  //     mainIconsTitles.quality[2]
-  //   );
-  //     testOne.renderIcon();
 };
-
 export default main_icons_module;
